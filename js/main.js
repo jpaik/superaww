2dc193c29bef8338d8cec440d11ef20a4011a049
James Paik <jpaik456@gmail.com>
2016-06-23 03:48:51 -0400

    Add comments and remove console

--- js/main.js
+++ js/main.js
@@ -17,19 +17,19 @@ function getPosts(params){
           if(imgurl != "invalid"){
             postLink.push(post.data.url); //Add it to array of duplicate checks.
 
-            $("#content").append( '<br>' + post.data.title );
-            $("#content").append( '<br>' + post.data.url);
+            $("#content").append( '<br>' + post.data.title ); //Get Post Title
+            $("#content").append( '<br>' + post.data.url); //Get Post Direct Link Raw URL
 
-            if($.inArray(imgext, ['jpg','png','jpeg','tif']) > -1 ){
+            if($.inArray(imgext, ['jpg','png','jpeg','tif']) > -1 ){ //If the post already has a picture extension
               $("#content").append( '<br>' + '<img class="image" src="' + imgurl + '">' );
             }
-            else if($.inArray("reddituploads", imgurl.split('.')) > -1 ){
+            else if($.inArray("reddituploads", imgurl.split('.')) > -1 ){ //If the picture is a reddit upload instead
               $("#content").append( '<br>' + '<img class="image" src="' + imgurl + '">' );
             }
-            else if($.inArray(imgext, ['gifv']) > -1 ){
+            else if($.inArray(imgext, ['gifv']) > -1 ){ //If the post is a gif, make it a webm and if not, mp4
               $("#content").append( '<br>' + '<video class="image" preload="auto" autoplay loop>'+'<source src="' + imgurl.substring(0,imgurl.length-4) + 'webm" type="video/webm">'+'<source src="' + imgurl.substring(0,imgurl.length-4) + 'mp4">' + '</video>' );
             }
-            else{
+            else{ //Otherwise just add a .jpg extension to the end. Brute force.
               $("#content").append( '<br>' + '<img class="image" src="' + imgurl + '.jpg">' );
             }
 
@@ -37,22 +37,23 @@ function getPosts(params){
           }
         }
       );
-      if (pagechildren && pagechildren.length > 0) {
-        lastId = pagechildren[pagechildren.length - 1].data.id;
-        if($.isEmptyObject(params) == false){
-          currentId = pagechildren[pagechildren.length - 25].data.id;
-          console.log("Current ID Title: "+pagechildren[pagechildren.length - 25].data.title);
+      if (pagechildren && pagechildren.length > 0) { //If there are posts
+        lastId = pagechildren[pagechildren.length - 1].data.id; //Get the last post's id so we can load at that point during new page
+        if($.isEmptyObject(params) == false){ //If it's not the home page
+          currentId = pagechildren[pagechildren.length - 25].data.id; //Then we can use this to load the next 5 images of the same page.
+        //  console.log("Current ID Title: "+pagechildren[pagechildren.length - 25].data.title);
         }
-        console.log("Next ID Title: "+pagechildren[pagechildren.length - 1].data.title);
-        console.log("Loaded LastID: " + lastId);
-        console.log("Loaded CurrentID: " +currentId);
+        //console.log("Next ID Title: "+pagechildren[pagechildren.length - 1].data.title);
+        //console.log("Loaded LastID: " + lastId);
+        //console.log("Loaded CurrentID: " +currentId);
       } else {
-        lastId = undefined;
+        lastId = undefined; //Hopefully this doesn't happen and we run out of posts
       }
     }
   );
 }
 
+//Init the function
 getPosts();
 
 //Check to make sure the image is served in HTTPS, is linked correctly
@@ -77,9 +78,7 @@ function fixURL(url){
 
 //Make sure it's not a duplicate, album, text post, instagram, or youtube video. (For now)
 function checkSource(imgurl){
-  console.log("Ran checkSource");
   if($.inArray(imgurl, postLink) > -1){
-    console.log("Duplicate");
     return false;
   }
   if(imgurl.indexOf("reddit.com") > -1 || imgurl.indexOf("youtube.com") > -1 || imgurl.indexOf("instagram.com") > -1){
@@ -96,19 +95,16 @@ function checkSource(imgurl){
 //Load More images when clicking load more.
 $('#loadMore').click(function(e){
   e.preventDefault();
-  loadVal += 5;
-  console.log("LoadVal: " + loadVal);
+  loadVal += 5; //Load only 5 at a time... For now?
   if(loadVal > 25){
     if(lastId){
       loadVal = 5;
-      console.log("Get New Page");
       getPosts({
         after: 't3_' + lastId
       });
     }
   }
   else{
-    console.log('LastID: '+ lastId);
     if(currentId){
       getPosts({
         after: 't3_' + currentId
